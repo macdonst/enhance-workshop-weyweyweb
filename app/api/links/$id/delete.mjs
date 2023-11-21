2b4ab823f92f2fd2d4270285d8d8c239713e019c
macdonst <simon.macdonald@gmail.com>
2023-11-21 19:55:57 +0100

    mod6

---
+++ app/api/links/$id/delete.mjs
@@ -0,0 +1,29 @@
+// View documentation at: https://enhance.dev/docs/learn/starter-project/api
+import { deleteLink } from '../../../models/links.mjs'
+
+
+/**
+ * @type {import('@enhance/types').EnhanceApiFn}
+ */
+export async function post (req) {
+  const id = req.pathParameters?.id
+
+  const session = req.session
+  // eslint-disable-next-line no-unused-vars
+  let { problems: removedProblems, link: removed, ...newSession } = session
+  try {
+    let link = await deleteLink(id)
+    return {
+      session: newSession,
+      json: { link },
+      location: '/links'
+    }
+  }
+  catch (err) {
+    return {
+      session: { ...newSession, error: err.message },
+      json: { error: err.message },
+      location: '/links'
+    }
+  }
+}
---
+++ app/models/schemas/link.mjs
@@ -0,0 +1,20 @@
+export const Link = {
+  "id": "Link",
+  "type": "object",
+  "properties": {
+    "text": {
+      "type": "string",
+      "format": "text"
+    },
+    "url": {
+      "type": "string",
+      "format": "url"
+    },
+    "completed": {
+      "type": "boolean"
+    },
+    "key": {
+      "type": "string"
+    }
+  }
+}
